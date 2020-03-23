899ae97bed140339a7cad1856321eb96d2b0040b
cichoszewicz <cichoszewicz@gmail.com>
2020-03-23 12:03:15 +0100

    during implementing axios

--- src/components/PostListing.js
+++ src/components/PostListing.js
@@ -2,8 +2,26 @@ import React from 'react'
 import Post from './Post'
 import { connect } from 'react-redux'
 
-function PostListing(props) {
-    let sortedPosts = props.posts.sort((a, b) => (a.id < b.id) ? 1 : -1)
+const axios = require('axios');
+
+class PostListing extends React.Component{
+
+  // Make a request for a user with a given ID
+  componentDidMount(){axios.get('https://http://5e787906491e9700162de1aa.mockapi.io/api/blog/posts')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error + "jasnychuj");
+  })
+  .finally(function () {
+    // always executed
+  });}
+
+  render(){
+  let sortedPosts = this.props.posts.sort((a, b) => (a.id < b.id) ? 1 : -1)
   return <div className='post-listing'>
     {
       sortedPosts.map( post =>
@@ -16,6 +34,7 @@ function PostListing(props) {
         />)
     }
   </div>
+  }
 }
 
 function mapStateToProps(state) {
