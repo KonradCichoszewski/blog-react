c8022d5bd17fe1dc2e9867a49f2f8a4bc10536ac
Konrad <cichoszewicz@gmail.com>
2020-03-25 18:18:49 +0100

    added delete-post button

--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -8,9 +8,10 @@ function Post(props) {
         <h3>{ props.title }</h3>
         <p>{ props.created }</p>
         <div id="post-content">{ props.content }</div>
+        <p className="edit-post">Edit</p>
         <div id="erase-post" onClick={ () => {console.log(props.id + " -> deleted id"); props.delete(props.id) }}>
-          <p>Delete this post</p>
-        </div> 
+          <p>Delete</p>
+        </div>
     </div>
 }
 
